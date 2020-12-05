cb547213d11175a42dca56195c7c109ebf24cf4a
Nicholas <nicholasjalderton@gmail.com>
2020-12-05 15:20:26 +0000

    Day 5

--- AoC2020/Day5/Day5.cpp
+++ AoC2020/Day5/Day5.cpp
@@ -4,11 +4,35 @@
 namespace Day5 {
     std::vector<std::string> input = Helpers::ReadAllLines<std::string>("./Day5Input.txt");
 
+    std::set<int> GetSeatIDs(std::vector<std::string> input) {
+        std::set<int> seatIDs = {};
+        for (auto& boardpass : input) {
+            int row = (boardpass[0] == 'F' ? 0 : 64)
+                + (boardpass[1] == 'F' ? 0 : 32)
+                + (boardpass[2] == 'F' ? 0 : 16)
+                + (boardpass[3] == 'F' ? 0 : 8)
+                + (boardpass[4] == 'F' ? 0 : 4)
+                + (boardpass[5] == 'F' ? 0 : 2)
+                + (boardpass[6] == 'F' ? 0 : 1);
+            int col = (boardpass[7] == 'L' ? 0 : 4)
+                + (boardpass[8] == 'L' ? 0 : 2)
+                + (boardpass[9] == 'L' ? 0 : 1);
+            int ID = row * 8 + col;
+            seatIDs.insert(ID);
+        }
+        return seatIDs;
+    }
+
     int A(std::vector<std::string> input) {
-        return -1;
+        return *(GetSeatIDs(input)).rbegin();
     }
 
     int B(std::vector<std::string> input) {
-        return -1;
+        int currentID = A(input);
+        auto seatIDs = GetSeatIDs(input);
+        while (seatIDs.count(currentID)) {
+            currentID--;
+        }
+        return currentID;
     }
 }
--- AoC2020/Day5/Day5.h
+++ AoC2020/Day5/Day5.h
@@ -3,9 +3,12 @@
 
 #include <vector>
 #include <string>
+#include <set>
 
 namespace Day5 {
     extern std::vector<std::string> input;
+
+    std::set<int> GetSeatIDs(std::vector<std::string> input);
 
     int A(std::vector<std::string> input);
     int B(std::vector<std::string> input);
