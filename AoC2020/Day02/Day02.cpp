8738e2b71d93f58d1adcb955e45db39a784d4d3f
Nicholas <nicholasjalderton@gmail.com>
2020-12-11 10:52:55 +0000

    Tidy day 2

--- AoC2020/Day02/Day02.cpp
+++ AoC2020/Day02/Day02.cpp
@@ -1,45 +1,25 @@
 #include "Day02.h"
 #include "../Helpers/Helpers.h"
 
+#include <regex>
+
 namespace Day02 {
     std::vector<std::string> input = Helpers::ReadAllLines<std::string>("./Day02Input.txt");
 
     int A(std::vector<std::string> input) {
         int valid = 0;
+        std::regex parseRegex("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
         for (auto& pw : input) {
-            int step = 0;
-            int minCount = 0;
-            int maxCount = 0;
-            char c = '_';
+            std::sregex_iterator iter(pw.begin(), pw.end(), parseRegex);
+            std::sregex_iterator end;
+            int minCount = std::stoi((*iter)[1]);
+            int maxCount = std::stoi((*iter)[2]);
+            char searchChar = (*iter)[3].str()[0];
+            std::string password = (*iter)[4];
+
             int count = 0;
-            for (int i = 0; i < pw.length(); i++) {
-                if (step == 0) {
-                    // Get first number
-                    if (pw[i] == '-') {
-                        step++;
-                    } else {
-                        minCount = minCount * 10 + (pw[i] - '0');
-                    }
-                } else if (step == 1) {
-                    // Get second number
-                    if (pw[i] == ' ') {
-                        step++;
-                    } else {
-                        maxCount = maxCount * 10 + (pw[i] - '0');
-                    }
-                } else if (step == 2) {
-                    // Get character to search for
-                    if (pw[i] == ' ') {
-                        step++;
-                    } else {
-                        c = c == '_' ? pw[i] : c;
-                    }
-                } else {
-                    // Perform password check
-                    if (pw[i] == c) {
-                        count++;
-                    }
-                }
+            for (auto& c : password) {
+                if (c == searchChar) { count++; }
             }
             if (count >= minCount && count <= maxCount) {
                 valid++;
@@ -50,44 +30,16 @@ namespace Day02 {
 
     int B(std::vector<std::string> input) {
         int valid = 0;
+        std::regex parseRegex("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
         for (auto& pw : input) {
-            int step = 0;
-            int firstPos = 0;
-            int secondPos = 0;
-            char c = '_';
-            bool validPass = false;
-            int pwStart = 0;
-            for (int i = 0; i < pw.length(); i++) {
-                if (step == 0) {
-                    // Get first number
-                    if (pw[i] == '-') {
-                        step++;
-                    } else {
-                        firstPos = firstPos * 10 + (pw[i] - '0');
-                    }
-                } else if (step == 1) {
-                    // Get second number
-                    if (pw[i] == ' ') {
-                        step++;
-                    } else {
-                        secondPos = secondPos * 10 + (pw[i] - '0');
-                    }
-                } else if (step == 2) {
-                    // Get character to search for
-                    if (pw[i] == ' ') {
-                        step++;
-                        pwStart = i;
-                    } else {
-                        c = c == '_' ? pw[i] : c;
-                    }
-                } else {
-                    // Perform password check
-                    if (i - pwStart == firstPos || i - pwStart == secondPos) {
-                        validPass ^= pw[i] == c;
-                    }
-                }
-            }
-            if (validPass) {
+            std::sregex_iterator iter(pw.begin(), pw.end(), parseRegex);
+            std::sregex_iterator end;
+            int firstPos = std::stoi((*iter)[1]);
+            int secondPos = std::stoi((*iter)[2]);
+            char searchChar = (*iter)[3].str()[0];
+            std::string password = (*iter)[4];
+
+            if ((password[firstPos - 1] == searchChar) ^ (password[secondPos - 1] == searchChar)) {
                 valid++;
             }
         }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2020
 
 Days to revisit:
- - 2 - Generally shit, switch to regexes
  - 4 - Resolve trailing new lines in input file
  - 7 - Improve A, maybe B as well, kinda slow
+ - 8 - Try improve B
